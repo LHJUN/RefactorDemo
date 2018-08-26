bf0aab5688a4b09479bda0f68f74f37446631a80
Lance Li <lance.li@oocl.com>
2018-08-26 23:13:40 +0800

    抽离switch case 到方法

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -27,24 +27,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDayRented() > 2) {
-                        thisAmount += (each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDayRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDayRented() > 3) {
-                        thisAmount += (each.getDayRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+            thisAmount = calculateAmount(each);
 
             //add frequent renter points
             frequentRenterPoints++;
@@ -62,5 +45,27 @@ public class Customer {
         result += "Amount owed is" + String.valueOf(totalAmount) + "\n";
         result += "You earned" + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+    private double calculateAmount(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDayRented() > 2) {
+                    thisAmount += (each.getDayRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDayRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDayRented() > 3) {
+                    thisAmount += (each.getDayRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
