0b4f6bf482c7bf74cbcceeca82348df80a78c7ea
Lance Li <lance.li@oocl.com>
2018-08-26 23:17:01 +0800

    移动calAmount方法到Rental类中并改名为getCharge

--- src/main/java/rentalstore/Rental.java
+++ src/main/java/rentalstore/Rental.java
@@ -16,4 +16,26 @@ public class Rental {
     public int getDayRented() {
         return dayRented;
     }
+
+    public double getCharge(){
+        double thisAmount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDayRented() > 2) {
+                    thisAmount += (getDayRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDayRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDayRented() > 3) {
+                    thisAmount += (getDayRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
 }
